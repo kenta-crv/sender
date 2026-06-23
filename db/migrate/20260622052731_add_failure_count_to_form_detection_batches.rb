74d6bc3936f6211b697d8b5fe2513cb11906f602
kenta-crv <okuyamakenta@gmail.com>
2026-06-23 14:09:00 +0900

    fix: migration files

--- db/migrate/20260622052731_add_failure_count_to_form_detection_batches.rb
+++ db/migrate/20260622052731_add_failure_count_to_form_detection_batches.rb
@@ -1,6 +1,5 @@
 class AddFailureCountToFormDetectionBatches < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :form_detection_batches, :success_count, from: nil, to: 0
-    change_column_default :form_detection_batches, :processed_count, from: nil, to: 0
+    add_column :form_detection_batches, :failure_count, :integer, default: 0
   end
 end
--- db/migrate/20260622053123_add_defaults_to_form_detection_batches.rb
+++ db/migrate/20260622053123_add_defaults_to_form_detection_batches.rb
@@ -1,4 +1,6 @@
 class AddDefaultsToFormDetectionBatches < ActiveRecord::Migration[6.1]
   def change
+    change_column_default :form_detection_batches, :success_count, from: nil, to: 0
+    change_column_default :form_detection_batches, :processed_count, from: nil, to: 0
   end
 end
